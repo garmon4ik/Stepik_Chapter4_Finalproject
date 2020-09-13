a0c16ce8c5cda0fadb4c25b9f47e3d53f72c3908
garmon4ik <uralenergostroi@gmail.com>
2020-09-13 11:26:15 +0500

    login page check transfered to basepage class

--- test_product_page.py
+++ test_product_page.py
@@ -46,3 +46,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.is_dissapeared(*ProductPageLocators.SUCCESS_MESSAGE)
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasePage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_login_page()
